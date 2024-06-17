cf780385b96eb9685ba42aa36167f2ae0392f136
rajsinghtech <rajsinghcpre@gmail.com>
2024-06-16 22:41:21 -0500

    install kubectl and fluxcd

--- apps/code-server/startup.sh
+++ apps/code-server/startup.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 echo "Running prestart script..."
 # Add your startup commands here
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -s https://fluxcd.io/install.sh | sudo FLUX_VERSION=2.0.0 bash
