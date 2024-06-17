729ca4e027f2ab00dd71fad62b811496da0b2659
rajsinghtech <rajsinghcpre@gmail.com>
2024-06-16 22:59:06 -0500

    Update script

--- apps/code-server/startup.sh
+++ apps/code-server/startup.sh
@@ -7,9 +7,10 @@ echo ' \ \_\ \_\  \ \_\ \_\ /\_____\     \/\_____\  \ \_\  \ \_\\"\_\  \ \_____\
 echo '  \/_/ /_/   \/_/\/_/ \/_____/      \/_____/   \/_/   \/_/ \/_/   \/_____/   \/_/\/_/ '
                                                                                      
 # Add your startup commands here
+echo '------'
+echo 'Installing Kubectl'
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+echo '------'
+echo 'Installing Fluxcd'
 curl -s https://fluxcd.io/install.sh | sudo FLUX_VERSION=2.0.0 bash
-test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
-test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.bashrc
-brew install k9s
