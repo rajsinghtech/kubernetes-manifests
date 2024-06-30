757eb1aa0e1ed048e44fcbd721fe4cf0fa4699ef
rajsinghtech <rajsingh@iastate.edu>
2024-06-30 11:23:12 -0500

    Add kubectl alias

--- apps/code-server/startup.sh
+++ apps/code-server/startup.sh
@@ -11,6 +11,10 @@ echo '------'
 echo 'Installing Kubectl'
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+kubectl completion bash | sudo tee /etc/bash_completion.d/kubectl > /dev/null
+sudo chmod a+r /etc/bash_completion.d/kubectl
+echo 'alias k=kubectl' >>~/.bashrc
+echo 'complete -o default -F __start_kubectl k' >>~/.bashrc
 echo '------'
 echo 'Installing Fluxcd'
 curl -s https://fluxcd.io/install.sh | FLUX_VERSION=2.0.0 bash
