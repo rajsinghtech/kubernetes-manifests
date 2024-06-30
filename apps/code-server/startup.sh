4e48cd6f2c450077aaa5d161451e3b52e686512e
rajsinghtech <rajsingh@iastate.edu>
2024-06-30 11:21:52 -0500

    Update apt

--- apps/code-server/startup.sh
+++ apps/code-server/startup.sh
@@ -15,6 +15,7 @@ echo '------'
 echo 'Installing Fluxcd'
 curl -s https://fluxcd.io/install.sh | FLUX_VERSION=2.0.0 bash
 echo '------'
+apt update
 apt install dnsutils iputils-ping -y
 # echo 'Installing Tailscale'
 # curl -fsSL https://tailscale.com/install.sh | sh
