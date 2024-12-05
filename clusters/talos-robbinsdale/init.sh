7498f502c7388d40c4528e42a7bf61fd4ef9ce37
Raj Singh <raj.singh@isovalent.com>
2024-12-04 22:07:42 -0500

    upgrade talos

---
+++ clusters/talos-robbinsdale/init.sh
@@ -0,0 +1,7 @@
+kubectl create ns monitoring
+kubectl create ns 1password
+kustomize build clusters/talos-robbinsdale/apps/1password --enable-helm | kubectl apply --server-side -f - || true
+kustomize build clusters/talos-robbinsdale/apps/monitoring --enable-helm | kubectl apply --server-side -f - || true
+kustomize build clusters/talos-robbinsdale/apps/cilium --enable-helm | kubectl apply --server-side -f - 
+kustomize build clusters/talos-robbinsdale/apps/argo-cd --enable-helm | kubectl apply --server-side -f - 
+kubectl apply -f clusters/talos-robbinsdale/apps/argo-cd/apps/argo-cd.yaml
