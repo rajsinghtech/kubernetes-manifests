5e823baba9db89e6b8d0821033efdd5210a47507
Raj Singh <raj.singh@isovalent.com>
2024-12-06 18:44:14 -0500

    cleanup

--- clusters/talos-robbinsdale/init.sh
+++ clusters/talos-robbinsdale/init.sh
@@ -1,7 +1,9 @@
 kubectl create ns monitoring
 kubectl create ns 1password
+kubectl create ns argocd
 kustomize build clusters/talos-robbinsdale/apps/1password --enable-helm | kubectl apply --server-side -f - || true
 kustomize build clusters/talos-robbinsdale/apps/monitoring --enable-helm | kubectl apply --server-side -f - || true
 kustomize build clusters/talos-robbinsdale/apps/cilium --enable-helm | kubectl apply --server-side -f - 
 kustomize build clusters/talos-robbinsdale/apps/argo-cd --enable-helm | kubectl apply --server-side -f - 
 kubectl apply -f clusters/talos-robbinsdale/apps/argo-cd/apps/argo-cd.yaml
+kubectl apply -f clusters/talos-robbinsdale/configs/localredirects.yaml
