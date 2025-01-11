7e22e4caae91441a8ef2352b9014728ac431713b
Raj Singh <raj.singh@isovalent.com>
2025-01-11 00:59:33 -0600

    update

--- clusters/talos-robbinsdale/init.sh
+++ clusters/talos-robbinsdale/init.sh
@@ -3,6 +3,7 @@ kubectl create ns 1password
 kubectl create ns argocd
 kubectl create ns envoy-gateway-system
 kustomize build clusters/talos-robbinsdale/apps/1password --enable-helm | kubectl apply --server-side -f - || true
+open "https://start.1password.com/open/i?a=IDFR3VM7VNC2XCMUGMW3HC377I&v=gri6pniqnwoqzyegmgv6hflcuy&i=eu7ouubv742a77667lzuvjncxy&h=my.1password.com"
 kustomize build clusters/talos-robbinsdale/apps/envoy-gateway-system --enable-helm | kubectl apply --server-side -f - || true
 kustomize build clusters/talos-robbinsdale/apps/monitoring --enable-helm | kubectl apply --server-side -f - || true
 kustomize build clusters/talos-robbinsdale/apps/cilium --enable-helm | kubectl apply --server-side -f - 
