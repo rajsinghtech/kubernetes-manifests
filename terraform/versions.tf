5bf652db4786292f7a5371e721aff45f70aab60f
Raj Singh <raj@tailscale.com>
2025-07-24 23:33:39 -0500

    Add Flux bootstrap resources and SOPS GPG key configuration
    - Introduced Flux bootstrap process in Terraform with a new namespace and resource definitions.
    - Added SOPS GPG key variable for secret decryption in Terraform.
    - Updated kustomization.yaml to include common Flux resources.
    - Enhanced Terraform provider configurations for kubectl and null resources.

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -14,6 +14,18 @@ terraform {
       source  = "hashicorp/helm"
       version = "~> 2.10"
     }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = "~> 1.14"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.2"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = "~> 0.9"
+    }
   }
 }
 
@@ -42,5 +54,17 @@ provider "helm" {
       command     = "aws"
       args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
     }
+  }
+}
+
+provider "kubectl" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  load_config_file       = false
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
   }
 } 
